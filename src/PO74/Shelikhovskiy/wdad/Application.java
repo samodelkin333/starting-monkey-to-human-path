4d1fa164e114ce23afa7d4fd74dc57f72e36bbf5
Константин <kostyakotk@mail.ru>
2019-09-26 12:24:26 +0300

    Added entry point

--- src/PO74/Shelikhovskiy/wdad/Application.java
+++ src/PO74/Shelikhovskiy/wdad/Application.java
@@ -1,4 +1,7 @@
 package PO74.Shelikhovskiy.wdad;
 
 public class Application {
+    public static void main(String[] args){
+        System.out.println("«I’m Константин Шелиховский, and I’m not a monkey");
+    }
 }
